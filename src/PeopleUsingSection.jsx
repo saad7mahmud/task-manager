225bf8d0e4074c8c4707b10174a1e7976a6fb881
saad7mahmud <saad7mahmud@gmail.com>
2023-12-22 19:25:35 +0600

    final

--- src/PeopleUsingSection.jsx
+++ src/PeopleUsingSection.jsx
@@ -7,22 +7,22 @@ const PeopleUsingSection = () => {
       <div className="container mx-auto text-center">
         <h2 className="text-3xl font-semibold mb-8">People Using</h2>
 
-        <div className="flex flex-wrap justify-center space-x-12">
+        <div className="flex flex-wrap items-center  justify-center space-x-12">
           {/* Developer */}
-          <div className="flex flex-col items-center">
-            <FaCode className="text-7xl text-blue-500 mb-2" />
-            <p className="text-xl">Developers</p>
+          <div className="flex justify-center flex-col items-center">
+            <FaCode className="text-xl text-blue-500 mb-2" />
+            <p className="text-xl text-center">Developers</p>
           </div>
 
           {/* Corporate Professionals */}
-          <div className="flex flex-col items-center">
-            <FaBriefcase className="text-7xl text-green-500 mb-2" />
-            <p className="text-xl">Corporate Professionals</p>
+          <div className="flex justify-center flex-col items-center">
+            <FaBriefcase className="text-xl text-green-500 mb-2" />
+            <p className="text-xl text-center">Corporates</p>
           </div>
 
           {/* Bankers */}
-          <div className="flex flex-col items-center">
-            <FaUniversity className="text-7xl text-indigo-500 mb-2" />
+          <div className="flex justify-center flex-col items-center">
+            <FaUniversity className="text-xl text-indigo-500 mb-2" />
             <p className="text-xl">Bankers</p>
           </div>
         </div>
